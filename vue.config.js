c3571b255d29cf05537a90620dd449d6088ec04b
WangCheng4934 <493475850@qq.com>
2020-07-27 15:09:38 +0800

    add publicPath

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
   devServer: {
     proxy: {
       // proxy all requests starting with /api to jsonplaceholder
